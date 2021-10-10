559ae2b202925cf8aab0b6bb79f44233227e21bc
Gilbertly <gilbert@qubitping.com>
2021-10-10 12:49:34 +0300

    refactor: init context in comment type

--- .github/actions/comment/src/index.js
+++ .github/actions/comment/src/index.js
@@ -1,13 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const commentIssue = (octokit, context, message) => {
+const commentIssue = (context, token, message) => {
   if (context.payload.issue == null) {
     core.setFailed('No issue found.');
     return;
   }
 
   const issueNumber = context.payload.issue.number;
+  const octokit = new github.getOctokit(token);
 
   octokit.issues.createComment({
     ...context.repo,
@@ -16,12 +17,13 @@ const commentIssue = (octokit, context, message) => {
   });
 };
 
-const commentPullRequest = (octokit, context, message) => {
+const commentPullRequest = (context, token, message) => {
   if (context.payload.pull_request == null) {
     core.setFailed('No pull_request found.');
     return;
   }
 
+  const octokit = new github.getOctokit(token);
   const prNumber = context.payload.pull_request.number;
 
   octokit.issues.createComment({
@@ -33,6 +35,7 @@ const commentPullRequest = (octokit, context, message) => {
 
 const run = async () => {
   try {
+    const context = github.context;
     const token = core.getInput('token');
     const type = core.getInput('type');
     const message = core.getInput('message');
@@ -42,12 +45,8 @@ const run = async () => {
       return;
     }
 
-    // eslint-disable-next-line new-cap
-    const octokit = new github.getOctokit(token);
-    const context = github.context;
-
-    if (type === 'issue') commentIssue(octokit, context, message);
-    if (type === 'pr') commentPullRequest(octokit, context, message);
+    if (type === 'issue') commentIssue(context, token, message);
+    if (type === 'pr') commentPullRequest(context, token, message);
   } catch (error) {
     core.setFailed(error.message);
   }
